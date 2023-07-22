fa140ece5b2e0ad26fec5ef831673956bad5facb
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-07-22 11:16:57 +0330

    آپدیت متد دیلیت و اکتیویت

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -11,9 +11,9 @@ using Microsoft.AspNetCore.Mvc;
 [ApiController]
 [Route("api/[Area]/todo")]
 public class MyController : Controller
-{   
+{
     [HttpGet("[action]")]
-    public  IActionResult GetAll()
+    public IActionResult GetAll()
     {
         var connection = MysqlConnect.GetConnection();
         using var cmd = connection.CreateCommand();
@@ -21,44 +21,45 @@ public class MyController : Controller
                             FROM Items i
                             INNER JOIN Status s ON i.StatusID = s.StatusID
                             INNER JOIN Priority p ON i.PriorityID = p.PriorityID ;";
-                            
-        List<TodoItem> items = new List<TodoItem>(); 
-        using( var reader = cmd.ExecuteReader())
+
+        List<TodoItem> items = new List<TodoItem>();
+        using (var reader = cmd.ExecuteReader())
         {
 
-                    while(reader.Read())
-                    {
-                   
-                    Console.WriteLine(string.Format(
-                    "Reading from table=({0}, {1}, {2},{3},{4},{5},{6},{7},{8},{9},{10})",
-                    reader.GetInt32(0),
-                    reader.GetString(1),
-                    reader.GetBoolean(2),
-                    reader.GetBoolean(3),
-                    reader.GetDateTime(4),
-                    reader.GetDateTime(5),
-                    reader.GetInt32(6),
-                    reader.GetString(7),
-                    reader.GetString(8),                    
-                    reader.GetInt32(9),
-                    reader.GetString(10)                   
-                
-                    ));
-                    items.Add(new TodoItem {
-                                            ItemsID = reader.GetInt32(reader.GetOrdinal("ItemsID")),
-                                            Name = reader.GetString(reader.GetOrdinal("name")),
-                                            IsComplete = reader.GetBoolean(reader.GetOrdinal("IsCompelete")),
-                                            IsDelete = reader.GetBoolean(reader.GetOrdinal("IsDelete")),
-                                            Created_At = reader.GetDateTime(reader.GetOrdinal("Created_At")),
-                                            Update_At = reader.GetDateTime(reader.GetOrdinal("Update_At")),                                                                      
-                                            PriorityID = reader.GetInt32(reader.GetOrdinal("PriorityID")),
-                                            Title = reader.GetString(reader.GetOrdinal("Title")),
-                                            Description = reader.GetString(reader.GetOrdinal("Description")),
-                                            StatusID = reader.GetInt32(reader.GetOrdinal("StatusID")),
-                                            TitleStatus = reader.GetString(reader.GetOrdinal("TitleStatus"))                                            
-                                            });
-                    }
-                    
+            while (reader.Read())
+            {
+
+                Console.WriteLine(string.Format(
+                "Reading from table=({0}, {1}, {2},{3},{4},{5},{6},{7},{8},{9},{10})",
+                reader.GetInt32(0),
+                reader.GetString(1),
+                reader.GetBoolean(2),
+                reader.GetBoolean(3),
+                reader.GetDateTime(4),
+                reader.GetDateTime(5),
+                reader.GetInt32(6),
+                reader.GetString(7),
+                reader.GetString(8),
+                reader.GetInt32(9),
+                reader.GetString(10)
+
+                ));
+                items.Add(new TodoItem
+                {
+                    ItemsID = reader.GetInt32(reader.GetOrdinal("ItemsID")),
+                    Name = reader.GetString(reader.GetOrdinal("name")),
+                    IsComplete = reader.GetBoolean(reader.GetOrdinal("IsCompelete")),
+                    IsDelete = reader.GetBoolean(reader.GetOrdinal("IsDelete")),
+                    Created_At = reader.GetDateTime(reader.GetOrdinal("Created_At")),
+                    Update_At = reader.GetDateTime(reader.GetOrdinal("Update_At")),
+                    PriorityID = reader.GetInt32(reader.GetOrdinal("PriorityID")),
+                    Title = reader.GetString(reader.GetOrdinal("Title")),
+                    Description = reader.GetString(reader.GetOrdinal("Description")),
+                    StatusID = reader.GetInt32(reader.GetOrdinal("StatusID")),
+                    TitleStatus = reader.GetString(reader.GetOrdinal("TitleStatus"))
+                });
+            }
+
         }
 
         return Ok(items.OrderBy(x => x.ItemsID));
@@ -71,12 +72,13 @@ public class MyController : Controller
         var connection = MysqlConnect.GetConnection();
         using var cmd = connection.CreateCommand();
         cmd.CommandText = $"SELECT COUNT(*) FROM Items WHERE ItemsID = @id;";
-        cmd.Parameters.AddWithValue("@id",id);
+        cmd.Parameters.AddWithValue("@id", id);
         var count = await cmd.ExecuteScalarAsync();
-        if(count != null)
+        if (count != null)
         {
             var number = (long)count;
-            if(number == 0){
+            if (number == 0)
+            {
                 return NotFound($"رکوردی با این شماره آیدی وجود ندارد:{id}");
             }
         }
@@ -88,34 +90,35 @@ public class MyController : Controller
 
                             WHERE ItemsID=@id;";
         List<TodoItem> items = new List<TodoItem>();
-        using(var reader = cmd.ExecuteReader())
+        using (var reader = cmd.ExecuteReader())
         {
-            while(reader.Read())
+            while (reader.Read())
             {
-                items.Add(new TodoItem {
-                                        ItemsID = reader.GetInt32(reader.GetOrdinal("ItemsID")) , 
-                                        Name = reader.GetString(reader.GetOrdinal("name")), 
-                                        IsComplete = reader.GetBoolean(reader.GetOrdinal("IsCompelete")), 
-                                        IsDelete = reader.GetBoolean(reader.GetOrdinal("IsDelete")),
-                                        Created_At = reader.GetDateTime(reader.GetOrdinal("Created_At")),
-                                        PriorityID = reader.GetInt32(reader.GetOrdinal("PriorityID")),
-                                        Update_At = reader.GetDateTime(reader.GetOrdinal("Update_At")),  
-                                        Title = reader.GetString(reader.GetOrdinal("Title")),
-                                        Description = reader.GetString(reader.GetOrdinal("Description")),
-                                        TitleStatus = reader.GetString(reader.GetOrdinal("TitleStatus")),
-                                        StatusID = reader.GetInt32(reader.GetOrdinal("StatusID"))
+                items.Add(new TodoItem
+                {
+                    ItemsID = reader.GetInt32(reader.GetOrdinal("ItemsID")),
+                    Name = reader.GetString(reader.GetOrdinal("name")),
+                    IsComplete = reader.GetBoolean(reader.GetOrdinal("IsCompelete")),
+                    IsDelete = reader.GetBoolean(reader.GetOrdinal("IsDelete")),
+                    Created_At = reader.GetDateTime(reader.GetOrdinal("Created_At")),
+                    PriorityID = reader.GetInt32(reader.GetOrdinal("PriorityID")),
+                    Update_At = reader.GetDateTime(reader.GetOrdinal("Update_At")),
+                    Title = reader.GetString(reader.GetOrdinal("Title")),
+                    Description = reader.GetString(reader.GetOrdinal("Description")),
+                    TitleStatus = reader.GetString(reader.GetOrdinal("TitleStatus")),
+                    StatusID = reader.GetInt32(reader.GetOrdinal("StatusID"))
 
-                                        });
+                });
 
             }
         }
-        
+
         return Ok(items);
     }
 
 
     [HttpPost]
-    public async Task<IActionResult> Post(TodoItemPostModel model )
+    public async Task<IActionResult> Post(TodoItemPostModel model)
     {
         var connection = MysqlConnect.GetConnection();
         using var cmd = connection.CreateCommand();
@@ -128,34 +131,35 @@ public class MyController : Controller
 
 
         await cmd.ExecuteNonQueryAsync();
-        
+
         return Ok("ثبت با موفقیت انجام شد");
     }
 
-    
+
     [HttpPut]
     public async Task<IActionResult> Update(TodoItemUpdateModel model)
     {
         var connection = MysqlConnect.GetConnection();
         using var cmd = connection.CreateCommand();
         cmd.CommandText = $"SELECT COUNT(*) FROM Items WHERE ItemsID = @id;";
-        cmd.Parameters.AddWithValue("@id",model.ItemsID);
+        cmd.Parameters.AddWithValue("@id", model.ItemsID);
         var count = await cmd.ExecuteScalarAsync();
-        if(count != null)
+        if (count != null)
         {
             var number = (long)count;
-            if(number == 0){
+            if (number == 0)
+            {
                 return NotFound($"رکوردی با این شماره آیدی وجود ندارد:{model.ItemsID}");
             }
         }
 
         cmd.CommandText = @"UPDATE Items SET name = @name , Description = @Description, PriorityID = @PriorityID, StatusID = @StatusID, Update_At = @Update_At  WHERE ItemsID = @id;";
-        cmd.Parameters.AddWithValue("@name",model.Name);
-        cmd.Parameters.AddWithValue("@Description",model.Description);     
-        cmd.Parameters.AddWithValue("@PriorityID",model.PriorityID);
-        cmd.Parameters.AddWithValue("@StatusID",model.StatusID);
-        cmd.Parameters.AddWithValue("@Update_At",model.Update_At);
-        
+        cmd.Parameters.AddWithValue("@name", model.Name);
+        cmd.Parameters.AddWithValue("@Description", model.Description);
+        cmd.Parameters.AddWithValue("@PriorityID", model.PriorityID);
+        cmd.Parameters.AddWithValue("@StatusID", model.StatusID);
+        cmd.Parameters.AddWithValue("@Update_At", model.Update_At);
+
 
 
         cmd.ExecuteNonQuery();
@@ -169,52 +173,65 @@ public class MyController : Controller
         var connection = MysqlConnect.GetConnection();
         using var cmd = connection.CreateCommand();
         cmd.CommandText = $"SELECT COUNT(*) FROM Items WHERE ItemsID = @id;";
-        cmd.Parameters.AddWithValue("@id" , id);
+        cmd.Parameters.AddWithValue("@id", id);
         var count = await cmd.ExecuteScalarAsync();
-        if(count != null) {
-            var number = (long) count;
-            if(number.Equals(0))
+        if (count != null)
+        {
+            var number = (long)count;
+            if (number.Equals(0))
                 return NotFound($"No record found with ID: {id}");
         }
         try
         {
-            cmd.CommandText = @"UPDATE Items SET IsDelete = @isdelete WHERE ItemsID = @id;";
-            cmd.Parameters.AddWithValue("@isdelete" , true);
+            cmd.CommandText = @"UPDATEh Items SET IsDelete = @isdelete WHERE ItemsID = @id;";
+            cmd.Parameters.AddWithValue("@isdelete", true);
             await cmd.ExecuteNonQueryAsync();
         }
-        catch(Exception e){
-            Console.WriteLine(e.Message+"عملیات حذف ناموفق بود");
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message + "عملیات حذف ناموفق بود");
         }
-        
-        return Ok("حذف با موفقیت انجام شد");
+        finally
+        {
+            Console.WriteLine("حذف با موفقیت انجام شد");
+        }
+        return Ok();
     }
 
 
-    [HttpDelete]
+    [HttpDelete("[action]")]
     public async Task<IActionResult> Activate(TodoItemActivateMethod model)
     {
-        var connection =MysqlConnect.GetConnection();
+        var connection = MysqlConnect.GetConnection();
         using var cmd = connection.CreateCommand();
-        cmd.CommandText =$"SELECT COUNT(*) FROM Items WHERE ItemsID = @id;";
-        cmd.Parameters.AddWithValue("@id" , model.ItemsID);
+        cmd.CommandText = $"SELECT COUNT(*) FROM Items WHERE ItemsID = @id;";
+        cmd.Parameters.AddWithValue("@id", model.ItemsID);
         var count = await cmd.ExecuteScalarAsync();
-        if(count != null){
-            var number = (long) count;
-            if(number.Equals(0))
+        if (count != null)
+        {
+            var number = (long)count;
+            if (number.Equals(0))
             {
                 return NotFound($"رکوردی با این شماره آیدی یافت نشد:{model.ItemsID}");
             }
         }
         try
         {
-        cmd.CommandText = $"UPDATE Items SET IsDelete = @isdelete WHERE ItemsID = @id;";
-        cmd.Parameters.AddWithValue("IsDelete",model.IsDelete);
-        await cmd.ExecuteNonQueryAsync();
+            cmd.CommandText = $"UPDATE Items SET IsDelete = @isdelete WHERE ItemsID = @id;";
+            cmd.Parameters.AddWithValue("IsDelete", model.IsDelete);
+            await cmd.ExecuteNonQueryAsync();
         }
-        catch(Exception e){
+        catch (Exception)
+        {
+            return  StatusCode(StatusCodes.Status500InternalServerError);
 
-            Console.WriteLine(e.Message + "ناموفق بود");
+            // return Internal(e.Message + "ناموفق بود");
         }
+        finally
+        {
+            Console.WriteLine("Unsuccessful");
+        }
+        
         return Ok("عملیات با موفقیت انجام شد");
     }
 }
